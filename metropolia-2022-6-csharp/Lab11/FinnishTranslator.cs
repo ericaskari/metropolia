6e34393eaeb1150c444666adbb958b39b6ff89b0
Eric Askari <eric.askari@weare.fi>
2022-01-24 19:37:16 +0200

    Lab11

--- metropolia-2022-6-csharp/Lab11/FinnishTranslator.cs
+++ metropolia-2022-6-csharp/Lab11/FinnishTranslator.cs
@@ -2,18 +2,12 @@ namespace Lab10;
 
 public class FinnishTranslator: Translator
 {
-    private Dictionary<string, string> _dictionary = new();
 
     public FinnishTranslator()
     {
-        _dictionary.Add("apua", "help");
-        _dictionary.Add("punainen", "red");
-        _dictionary.Add("sininen", "blue");
-        _dictionary.Add("vihreä", "green");
-    }
-
-    public new string Translate(string text)
-    {
-        return _dictionary.GetValueOrDefault(text.ToLower(), "-");
+        Dictionary.Add("apua", "help");
+        Dictionary.Add("punainen", "red");
+        Dictionary.Add("sininen", "blue");
+        Dictionary.Add("vihreä", "green");
     }
 }
--- metropolia-2022-6-csharp/Lab11/Translator.cs
+++ metropolia-2022-6-csharp/Lab11/Translator.cs
@@ -2,22 +2,9 @@ namespace Lab10;
 
 public abstract class Translator
 {
-    private readonly Dictionary<string, string> _dictionary = new();
-
-    public Translator()
-    {
-        _dictionary.Add("Apua", "Help");
-        _dictionary.Add("Punainen", "Red");
-        _dictionary.Add("Sininen", "Blue");
-        _dictionary.Add("Vihreä", "Green");
-    }
-
+    protected readonly Dictionary<string, string> Dictionary = new();
     public string Translate(string text)
     {
-        string result = "";
-
-        _dictionary.TryGetValue(text, out result);
-
-        return result;
+        return Dictionary.GetValueOrDefault(text.ToLower(), "-");
     }
 }
